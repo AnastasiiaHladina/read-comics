186c5fb5c2b60263b1c60acfd6d077426fc2308c
Anastasiia Hladina <anastasiakaramelle@gmail.com>
2020-03-23 19:07:47 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {Routes, RouterModule} from '@angular/router';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ListPageComponent } from './list-page/list-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+// определение маршрутов
+const appRoutes: Routes = [
+  { path: '', component: MainPageComponent},
+  { path: 'list', component: ListPageComponent},
+  { path: '**', component: NotFoundPageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    ListPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
   ],
   providers: [],
